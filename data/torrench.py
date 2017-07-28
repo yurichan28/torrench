0801a71658bf053b3a2f2c6e2d68af0b46b68a91
Ricky Wilson <echoquote@gmail.com>
2017-07-28 00:27:32 -0500

    Fixed some formatting issues

--- data/torrench.py
+++ data/torrench.py
@@ -23,8 +23,31 @@ import os
 import sys
 import argparse
 
+try:
+	import requests
+	import time
+	import platform
+	from bs4 import BeautifulSoup
+	from tabulate import tabulate
+	from termcolor import colored
+	OS_WIN = False
+	if platform.system() == 'Windows': #Determine platform
+		import colorama
+		import ctypes
+		from multiprocessing import Queue
+		colorama.init()
+		os.system("mode CON: COLS=180 LINES=300")
+		ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 3)
+		OS_WIN = True
+
+except ImportError as e:
+	print(e)
+	print("Please install and retry.\n")
+	sys.exit()
+
+
 def init(args):
-    
+
     ''' Resolve the command line arguments.
     :param args: argparse.Namespace
     :rtype: None
@@ -36,54 +59,32 @@ def init(args):
     if args.clear_html:
         home = os.path.expanduser('~')
         temp_dir = home+"/.torrench/temp/"
-        files = os.listdir(temp_dir);
+        files = os.listdir(temp_dir)
 
         if not files:
             print("\nNothing to remove\n")
         # Remove temp files.
         else:
             for count, file_name in enumerate(files):
-                os.remove(os.path.join(temp_dir, file_name));
+                os.remove(os.path.join(temp_dir, file_name))
 
             print("\nRemoved {} file(s).\n".format(count))
         sys.exit(0)
 
     if input_title == None:
-        print("\nInput string expected.\nUse --help for more\n");
-        sys.exit(0);
+        print("\nInput string expected.\nUse --help for more\n")
+        sys.exit(0)
 
     elif page_limit <= 0 or page_limit > 50:
         print("Enter valid page input [0<p<=50]")
-        sys.exit(0);
+        sys.exit(0)
 
     else:
-        main(input_title, page_limit);
+        main(input_title, page_limit)
         # Resolve input arguments - END
 
 def main(input_title, page_limit):
-    try:
-        ## Adding imports here since they are only required if this function is called.
-        import requests
-        import time
-        import platform
-        from bs4 import BeautifulSoup
-        from tabulate import tabulate
-        from termcolor import colored
-        OS_WIN = False
-        if platform.system() == 'Windows': #Determine platform
-            import colorama;
-            import ctypes
-            from multiprocessing import Queue
-            colorama.init()
-            os.system("mode CON: COLS=180 LINES=300");
-            ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 3)
-            OS_WIN = True
-			
-    except ImportError as e:
-        print(e);
-        print("Please install and retry.\n")
-        sys.exit()
-
+    
     ## MAIN CODE EXECUTION BEGINS HERE
     try:
         # Get proxy list
@@ -110,11 +111,11 @@ def main(input_title, page_limit):
             #initially page_result_count=9999
             # If results in a page are <30, break loop (no more remaining pages are fetched)
             if page_result_count < 30:
-                break;
+                break
 
             if page_limit > 1:
                 fetch_status_str = "\nFetching from page: "+str(p+1)
-                print(fetch_status_str);
+                print(fetch_status_str)
             else:
                 print("Fetching results...")
 
@@ -133,9 +134,9 @@ def main(input_title, page_limit):
                     #print("[%.2f sec]" %())
                     time.sleep(1)
                     raw = raw.content
-                    break;
+                    break
                 except requests.exceptions.ConnectionError as e:
-                    print("Connection error... ");
+                    print("Connection error... ")
                     url_list_count += 1
                     url = url_list[url_list_count]
                     print("Trying "+url)
@@ -146,12 +147,13 @@ def main(input_title, page_limit):
                 content = soup.find_all('table', id="searchResult")[0]
             except IndexError:
                 if p == 0:
-                    print("\nNo results found for given input!");
+                    print("\nNo results found for given input!")
                     break
 
             data = content.find_all('tr')
             mylist = []
-            temp_url=url.split('//')[-1]
+            temp_url = url.split('//')[-1]
+
             ### Extraction begins here ###
             for i in data[1:]:
                 name = i.find('a', class_='detLink')
@@ -193,7 +195,7 @@ def main(input_title, page_limit):
                 leeches = i.find_all('td', align="right")[1].string
                 date = i.find('font', class_="detDesc").get_text().split(' ')[1].replace(',', "")
                 size = i.find('font', class_="detDesc").get_text().split(' ')[3].replace(',', "")
-                torr_id = i.find('a', {'class': 'detLink'})["href"].split('/')[2];
+                torr_id = i.find('a', {'class': 'detLink'})["href"].split('/')[2]
                 link = url+"/torrent/"+torr_id
                 magnet = i.find_all('a', {'title': 'Download this torrent using magnet'})[0]['href']
                 ### Extraction ends here ###
@@ -211,7 +213,7 @@ def main(input_title, page_limit):
             if(page_limit == 1):
                 total_page_fetch_time = page_fetch_time
             else:
-                print(">> "+str(page_result_count)+" torrents");
+                print(">> "+str(page_result_count)+" torrents")
                 print ("[%.2f sec]" %(page_fetch_time))
                 total_page_fetch_time += page_fetch_time
 
@@ -221,30 +223,30 @@ def main(input_title, page_limit):
 
     # Print Results and fetch torrent details
     if(total_result_count > 0):
-        print("\n\nS=Seeds; L=Leeches; C=Comments");
+        print("\n\nS=Seeds; L=Leeches; C=Comments")
         final_output = tabulate(masterlist, headers=['TYPE', 'NAME', 'INDEX', 'UPLOADER', 'SIZE','S','L', 'UPLOADED', "C"], tablefmt="grid")
         print(final_output)
-        print("\nTotal: "+str(total_result_count)+" torrents"+" [in %.2f sec]" %(total_page_fetch_time));
+        print("\nTotal: "+str(total_result_count)+" torrents"+" [in %.2f sec]" %(total_page_fetch_time))
         exact_no_of_pages = total_result_count//30
         has_extra_page = total_result_count%30
         if has_extra_page > 0:
             exact_no_of_pages +=1
-        print("Total pages: "+str(exact_no_of_pages));
+        print("Total pages: "+str(exact_no_of_pages))
         print("\nFurther, a torrent's details can be fetched (Description, comments, download(Magnetic) Link, etc.)");
 
         # Fetch torrent details
         import details
-        print("Enter torrent's index value to fetch details (Maximum one index)\n");
+        print("Enter torrent's index value to fetch details (Maximum one index)\n")
         option = 9999
         option2 = 0
         while(option != 0):
             try:
-                option = int(input("(0 = exit)\nindex > "));
+                option = int(input("(0 = exit)\nindex > "))
                 if option > total_result_count or option < 0 or option == "":
-                    print("**Enter valid index**\n\n");
-                    continue;
+                    print("**Enter valid index**\n\n")
+                    continue
                 elif option == 0:
-                    break;
+                    break
                 else:
                     selected_link = details_link[str(option)]
                     selected_name = details_name[str(option)]
@@ -262,18 +264,18 @@ def main(input_title, page_limit):
                                 print(e)
                             continue
                         elif option2 == 'g':
-                            print("Fetching details for torrent index [%d] : %s" %(option, selected_name));
+                            print("Fetching details for torrent index [%d] : %s" %(option, selected_name))
                             file_url = details.get_details(selected_link, str(option))
                             file_url = colored(file_url, 'yellow')
-                            print("File URL: "+file_url+"\n\n");
+                            print("File URL: "+file_url+"\n\n")
                     else:
                         print("Bad Input!\n")
                         continue
             except KeyboardInterrupt:
-                break;
+                break
             except ValueError:
-                print("Check input! (Enter one (integer) index at a time)\n\n");
-        print("\nBye!\n");
+                print("Check input! (Enter one (integer) index at a time)\n\n")
+        print("\nBye!\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A simple torrent search tool.")
@@ -282,4 +284,5 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--clear-html", action="store_true", default=False, help="Clear all torrent description HTML files and exit.")
     parser.add_argument("-v", "--version", action='version', version='%(prog)s v1.3', help="Display version and exit.")
     args = parser.parse_args()
-    init(args);
+    init(args)
+

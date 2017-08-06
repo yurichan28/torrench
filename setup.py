b6215b5baecf61af48e5320544f08e4657e04aa0
kryptxy <kryptxy@protonmail.com>
2017-08-06 08:49:47 +0530

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+
+try:
+	from setuptools import setup
+except ImportError:
+	print("Missing package setuptools. Please install to continue\n(https://pypi.python.org/pypi/setuptools)")
+	
+DESCRIPTION = 'Command-line torrent search tool for Windows and Linux OS'
+LONG_DESCRIPTION = 'Program to search and download torrents from existing torrent-hosting sites.'
+VERSION = '1.0.0.20170806'
+
+setup(
+    name = "torrench",
+    version = VERSION,
+    author = "Rijul Gulati",
+    author_email = "kryptxy@protonmail.com",
+    description = (DESCRIPTION),
+    license = "GPL",
+    url = "https://github.com/kryptxy/torrench",
+    packages=['torrench', 'torrench.tpb', 'torrench.linuxtracker'],
+    install_requires=['bs4','lxml','requests','tabulate','termcolor'],
+    long_description=(LONG_DESCRIPTION),
+    entry_points = {'console_scripts': ['torrench = torrench.__main__:main']},
+    zip_safe = False,
+    keywords = ['torrents', 'thepiratebay', 'linuxtracker'], 
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
+    ],
+)

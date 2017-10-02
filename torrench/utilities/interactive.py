201b2f2c8e96589cc47ac60b110eb896a4c54834
kryptxy <kryptxy@protonmail.com>
2017-10-02 10:51:04 +0530

    Added log messages

--- torrench/utilities/interactive.py
+++ torrench/utilities/interactive.py
@@ -1,5 +1,6 @@
-from sys import exit as _exit
 
+import logging
+from sys import exit as _exit
 import torrench.modules.distrowatch as distrowatch
 import torrench.modules.kickasstorrent as kat
 import torrench.modules.linuxtracker as linuxtracker
@@ -18,6 +19,7 @@ class InteractiveMode:
     """
     def __init__(self):
         self._modules = {}
+        self.logger = logging.getLogger('log1')
 
     def parser(self, query):
         """
@@ -25,13 +27,16 @@ class InteractiveMode:
         """
         _available_modules = self._set_modules().keys()
         if query[:4] in ('!h', 'help'):
+            self.logger.debug("Display !h (help menu)")
             self._interactive_help()
         elif query[:2] in _available_modules:
             self._caller(query[:2], query[3:])
         elif query[:4] in ('!q', 'quit'):
+            self.logger.debug("!q selected. Exiting interactive mode")
             print("Bye!")
             _exit(2)
         else:
+            self.logger.debug("Invalid command input")
             print('Invalid command! Try `!h` or `help` for help.')
 
     def _set_modules(self):
@@ -39,19 +44,23 @@ class InteractiveMode:
         Map functions to commands and return dictionary.
         """
         if Config().file_exists():
-            self._modules = {'!t': tpb_module,
-                             '!n': nyaa_module,
-                             '!k': kat,
-                             '!x': xbit_module,
-                             '!d': distrowatch,
-                             '!l': linuxtracker,
-                             '!s': sky
+            self._modules = {
+                            '!t': tpb_module,
+                            '!n': nyaa_module,
+                            '!k': kat,
+                            '!x': xbit_module,
+                            '!d': distrowatch,
+                            '!l': linuxtracker,
+                            '!s': sky
                             }
             return self._modules
+        else:
+            self.logger.debug("Config file not setup!")
 
-        self._modules = {'!d': distrowatch,
-                         '!l': linuxtracker
-                        }
+        self._modules = {
+                            '!d': distrowatch,
+                            '!l': linuxtracker
+                            }
 
         return self._modules
 
@@ -64,12 +73,14 @@ class InteractiveMode:
         """
         _modules = self._set_modules()
         if query and module in _modules and not query.isspace():
+            self.logger.debug("Selected module %s, query: %s" % ((module), query))
             if module in ['!t', '!k', '!s']:
                 _modules[module].main(query, page_limit=1)
             else:
                 _modules[module].main(query)
         else:
             print("You called an invalid module or provided an empty query.")
+            self.logger.debug("Called an invalid module or provided an empty query.")
 
     @staticmethod
     def _interactive_help():
@@ -79,15 +90,18 @@ class InteractiveMode:
         help_text = """
             Available commands:
         !h or help  - Help text (this)
+        !q or quit  - Quit interactive mode
+        !t <string> - Search on LinuxTorrents
+        !d <string> - Search on DistroWatch
+
+        =========== Requires Config file ==========
         !n <string> - Search on nyaa.si for anime.
         !t <string> - Search on ThePirateBay.
         !k <string> - Search on KickAssTorrents.
         !s <string> - Search on SkyTorrents
         !x <string> - Search on XBit.pw
-        !t <string> - Search on LinuxTorrents
-        !q or quit  - Quit interactive mode
-
-        Some commands are only available after a `config.ini` file has been set.
+        ===========================================
+        These commands are only available after a `config.ini` file has been set.
         See the documentation for more information.
         """
         print(help_text)
@@ -101,9 +115,11 @@ def inter():
         i = InteractiveMode()
         while True:
             data = input(Config().colorify("yellow", '\ntorrench > '))
+            i.logger.debug(data)
             i.parser(data)
     except (KeyboardInterrupt, EOFError):
         print('Terminated.')
+        i.logger.debug("Terminated.")
 
 
 if __name__ == '__main__':

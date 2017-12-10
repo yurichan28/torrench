ca23454051d5b540e1898f608b76b3def5c84942
kryptxy <kryptxy@protonmail.com>
2017-12-10 18:07:52 +0530

    [Windows] Fix losing cursor after fullscreen cmd window

--- torrench/__main__.py
+++ torrench/__main__.py
@@ -12,15 +12,12 @@ logging.config.dictConfig(logger.LOG_SETTINGS)
 mylogger = logging.getLogger('log1')
 
 if platform.system() == 'Windows':
-    from multiprocessing import Queue
-    import ctypes
-    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 3)
-    # Expand windows console window so output does not overlap (font==default).
     os.system("mode 800")
 
 system = platform.platform()
 pyversion = sys.version
 arch = platform.machine()
+
 
 def main():
     """Execution begins here."""
